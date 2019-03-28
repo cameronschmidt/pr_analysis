037748a39c11fbf3d3feb78e03bdbe268eadcdb2
CameronSchmidt <cameron.schmidt@sonarsource.com>
2019-03-28 10:48:30 +0100

    Added several problems

--- src/main/java/pranalysis/App.java
+++ src/main/java/pranalysis/App.java
@@ -10,10 +10,16 @@ public class App
     		// TODO fix this too..
     		int j = 5 / i;
     	}
+      for (int k = 0; k < 10; k--)
+      {
+        System.out.println("In a loop!");
+      }
+      
     	System.out.println("IN AMETHOD"); 
     }
     
     public String uncoveredMethod() {
+      // TODO: need to fix this uncovered method
     	return "UNCOVERED";
     }
 }
